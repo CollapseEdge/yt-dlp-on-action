1d41d2eb18451f81e833986403966c0aeb6e4631
CollapseEdge <123354572+CollapseEdge@users.noreply.github.com>
2023-03-14 20:31:47 +0800

    Update clear.py

--- clear.py
+++ clear.py
@@ -1,5 +1,5 @@
 import os
-file = open('../download.txt','w');
-file = open("'../download.txt'", 'w').close()
-file = open('../log.txt','w');
-file = open("'../log.txt'", 'w').close()
+file = open('download.txt','w');
+file = open("'download.txt'", 'w').close()
+file = open('log.txt','w');
+file = open("'log.txt'", 'w').close()
